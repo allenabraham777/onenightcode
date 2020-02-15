788eeed138303d4b6ab5835dfba36f2265cd4f78
Allen K Abraham <allenabraham777@gmail.com>
2020-02-16 00:47:51 +0530

    Initial Commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Date.IO</title>
+</head>
+<body>
+    <!-- Start of Navbar -->
+    <nav>
+    
+    </nav>
+    <!-- End of Navbar -->
+</body>
+</html>
